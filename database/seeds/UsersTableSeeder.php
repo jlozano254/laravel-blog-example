aa1985d2f5cd9a5c9aa53a8aad811eb6b6e91179
Jesus Lozano <jesusemanuel.254@gmail.com>
2018-02-09 23:30:07 +0000

    feat: First commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'name' => 'Jesus Lozano',
+        ]);
+        factory(User::class, 30)->create();
+    }
+}
